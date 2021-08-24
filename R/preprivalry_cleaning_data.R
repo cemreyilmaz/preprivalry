435d836f378f6038a8aa8238400a7b2ed488205d
cemreyilmaz <cemreyilmaz@yahoo.com>
2021-08-24 16:32:34 +0200

    debugged cleaning functions

--- R/preprivalry_cleaning_data.R
+++ R/preprivalry_cleaning_data.R
@@ -106,41 +106,43 @@ clean_keyevents <- function(key,iteration){
 #' \dontrun{
 #' remove_irrelevant_keyevents(trial_key,percept_keys)}
 remove_irrelevant_keyevents <- function(key,percept_keys){
-  # first, remove irrelevant key events
-  key$idUp[!(key$idUp == percept_keys[1] | key$idUp == percept_keys[2])] <- NaN
-  key$timeUp[!(key$idUp == percept_keys[1] | key$idUp == percept_keys[2])] <- NaN
-  key$nameUp[!(key$idUp == percept_keys[1] | key$idUp == percept_keys[2])] <- NaN
-  key$idDown[!(key$idDown == percept_keys[1] | key$idDown == percept_keys[2])] <- NaN
-  key$timeDown[!(key$idDown == percept_keys[1] | key$idDown == percept_keys[2])] <- NaN
-  key$nameDown[!(key$idDown == percept_keys[1] | key$idDown == percept_keys[2])] <- NaN
   id_down <- key$idDown[!is.na(key['idDown'])]
   time_down <- key$timeDown[!is.na(key['idDown'])]
   name_down <- key$nameDown[!is.na(key['idDown'])]
   id_up <- key$idUp[!is.na(key['idUp'])]
   time_up <- key$timeUp[!is.na(key['idUp'])]
   name_up <- key$nameUp[!is.na(key['idUp'])]
-  if(length(id_down) < length(id_up)){
-    if(time_down[1]<time_up[1]){ # remove last release
-      id_up <- id_up[1:(length(id_up)-1)]
-      time_up <- time_up[1:(length(time_up)-1)]
-      name_up <- name_up[1:(length(name_up)-1)]
+  # first, remove irrelevant key events
+  id_down <- id_down[(id_down == percept_keys[1] | id_down == percept_keys[2])]
+  time_down <- time_down[(id_down == percept_keys[1] | id_down == percept_keys[2])]
+  name_down <- name_down[(id_down == percept_keys[1] | id_down == percept_keys[2])]
+  id_up <- id_up[(id_up == percept_keys[1] | id_up == percept_keys[2])]
+  time_up <- time_up[(id_up == percept_keys[1] | id_up == percept_keys[2])]
+  name_up <- name_up[(id_up == percept_keys[1] | id_up == percept_keys[2])]
+  for(i in 1:2){
+    if(length(id_down) < length(id_up)){
+      if(time_down[1]<time_up[1]){ # remove last release
+        id_up <- id_up[1:(length(id_up)-1)]
+        time_up <- time_up[1:(length(time_up)-1)]
+        name_up <- name_up[1:(length(name_up)-1)]
+      }
+      if(time_down[1]>time_up[1]){ # remove first release
+        id_up <- id_up[2:length(id_up)]
+        time_up <- time_up[2:length(time_up)]
+        name_up <- name_up[2:length(name_up)]
+      }
     }
-    if(time_down[1]>time_up[1]){ # remove first release
-      id_up <- id_up[2:length(id_up)]
-      time_up <- time_up[2:length(time_up)]
-      name_up <- name_up[2:length(name_up)]
-    }
-  }
-  if(length(id_down) > length(id_up)){
-    if(time_down[1]<time_up[1]){ # remove last press
-      id_down <- id_down[1:(length(id_down)-1)]
-      time_down <- time_down[1:(length(time_down)-1)]
-      name_down <- name_down[1:(length(name_down)-1)]
-    }
-    if(time_down[1]>time_up[1]){ # remove first press
-      id_down <- id_down[2:length(id_down)]
-      time_down <- time_down[2:length(time_down)]
-      name_down <- name_down[2:length(name_down)]
+    if(length(id_down) > length(id_up)){
+      if(time_down[1]<time_up[1]){ # remove last press
+        id_down <- id_down[1:(length(id_down)-1)]
+        time_down <- time_down[1:(length(time_down)-1)]
+        name_down <- name_down[1:(length(name_down)-1)]
+      }
+      if(time_down[1]>time_up[1]){ # remove first press
+        id_down <- id_down[2:length(id_down)]
+        time_down <- time_down[2:length(time_down)]
+        name_down <- name_down[2:length(name_down)]
+      }
     }
   }
   key <- data.frame(idDown = id_down, timeDown = time_down, nameDown = name_down,
