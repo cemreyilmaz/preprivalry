2a1eff1fc7a87b368ae3ec2f15089e129f545b51
cemreyilmaz <cemreyilmaz@yahoo.com>
2022-10-28 01:45:44 +0200

    correct syntax

--- R/preprivalry_cleaning_data.R
+++ R/preprivalry_cleaning_data.R
@@ -25,70 +25,70 @@ clean_keyevents <- function(key,iteration){
   id_down <- key$idDown
   time_down <- key$timeDown
   name_down <- key$nameDown
-  # cleaning
-  for(i in 1:iteration){
-    # if keyUp comes before first keyDown
-    # => delete first release
     tryCatch(
       {
-        if(time_up[1] < time_down[1]){
-          id_up   <- id_up[2:length(id_up)]
-          time_up   <- time_up[2:length(time_up)]
-          name_up   <- name_up[2:length(name_up)]
+        # cleaning
+        for(i in 1:iteration){
+          # if keyUp comes before first keyDown
+          # => delete first release
+          if(time_up[1] < time_down[1]){
+            id_up   <- id_up[2:length(id_up)]
+            time_up   <- time_up[2:length(time_up)]
+            name_up   <- name_up[2:length(name_up)]
+          }
+          # if keyUp and keyDown are not of the same percept at the beginning
+          # => delete first key event
+          if(id_up[1] != id_down[1]){
+            id_up <- id_up[2:length(id_up)]
+            time_up <- time_up[2:length(time_up)]
+            name_up <- name_up[2:length(name_up)]
+            id_down <- id_down[2:length(id_down)]
+            time_down <- time_down[2:length(time_down)]
+            name_down <- name_down[2:length(name_down)]
+          }
+          # if last keyDown comes after last keyUp
+          # => delete last press
+          if(time_down[length(time_down)] > time_up[length(time_up)]){
+            id_down <- id_down[1:(length(id_down)-1)]
+            time_down <- time_down[1:(length(time_down)-1)]
+            name_down <- name_down[1:(length(name_down)-1)]
+          }
+          # if keyUp and keyDown are not of the same percept at the end
+          # => delete last key event
+          if(id_up[length(id_up)] != id_down[length(id_down)]){
+            id_up <- id_up[1:(length(id_up)-1)]
+            time_up <- time_up[1:(length(time_up)-1)]
+            name_up <- name_up[1:(length(name_up)-1)]
+            id_down <- id_down[1:(length(id_down)-1)]
+            time_down <- time_down[1:(length(time_down)-1)]
+            name_down <- name_down[1:(length(name_down)-1)]
+          }
+        } # end
+        # check if there is a mismatch between the length of arrays
+        if(length(id_down) < length(id_up)){
+          if(time_down[1]<time_up[1]){ # remove last release
+            id_up <- id_up[1:(length(id_up)-1)]
+            time_up <- time_up[1:(length(time_up)-1)]
+            name_up <- name_up[1:(length(name_up)-1)]
+          }
+          if(time_down[1]>time_up[1]){ # remove first release
+            id_up <- id_up[2:length(id_up)]
+            time_up <- time_up[2:length(time_up)]
+            name_up <- name_up[2:length(name_up)]
+          }
         }
-        # if keyUp and keyDown are not of the same percept at the beginning
-        # => delete first key event
-        if(id_up[1] != id_down[1]){
-          id_up <- id_up[2:length(id_up)]
-          time_up <- time_up[2:length(time_up)]
-          name_up <- name_up[2:length(name_up)]
-          id_down <- id_down[2:length(id_down)]
-          time_down <- time_down[2:length(time_down)]
-          name_down <- name_down[2:length(name_down)]
+        if(length(id_down) > length(id_up)){
+          if(time_down[1]<time_up[1]){ # remove last press
+            id_down <- id_down[1:(length(id_down)-1)]
+            time_down <- time_down[1:(length(time_down)-1)]
+            name_down <- name_down[1:(length(name_down)-1)]
+          }
+          if(time_down[1]>time_up[1]){ # remove first press
+            id_down <- id_down[2:length(id_down)]
+            time_down <- time_down[2:length(time_down)]
+            name_down <- name_down[2:length(name_down)]
+          }
         }
-        # if last keyDown comes after last keyUp
-        # => delete last press
-        if(time_down[length(time_down)] > time_up[length(time_up)]){
-          id_down <- id_down[1:(length(id_down)-1)]
-          time_down <- time_down[1:(length(time_down)-1)]
-          name_down <- name_down[1:(length(name_down)-1)]
-        }
-        # if keyUp and keyDown are not of the same percept at the end
-        # => delete last key event
-        if(id_up[length(id_up)] != id_down[length(id_down)]){
-          id_up <- id_up[1:(length(id_up)-1)]
-          time_up <- time_up[1:(length(time_up)-1)]
-          name_up <- name_up[1:(length(name_up)-1)]
-          id_down <- id_down[1:(length(id_down)-1)]
-          time_down <- time_down[1:(length(time_down)-1)]
-          name_down <- name_down[1:(length(name_down)-1)]
-        }
-      } # end
-      # check if there is a mismatch between the length of arrays
-      if(length(id_down) < length(id_up)){
-        if(time_down[1]<time_up[1]){ # remove last release
-          id_up <- id_up[1:(length(id_up)-1)]
-          time_up <- time_up[1:(length(time_up)-1)]
-          name_up <- name_up[1:(length(name_up)-1)]
-        }
-        if(time_down[1]>time_up[1]){ # remove first release
-          id_up <- id_up[2:length(id_up)]
-          time_up <- time_up[2:length(time_up)]
-          name_up <- name_up[2:length(name_up)]
-        }
-      }
-      if(length(id_down) > length(id_up)){
-        if(time_down[1]<time_up[1]){ # remove last press
-          id_down <- id_down[1:(length(id_down)-1)]
-          time_down <- time_down[1:(length(time_down)-1)]
-          name_down <- name_down[1:(length(name_down)-1)]
-        }
-        if(time_down[1]>time_up[1]){ # remove first press
-          id_down <- id_down[2:length(id_down)]
-          time_down <- time_down[2:length(time_down)]
-          name_down <- name_down[2:length(name_down)]
-        }
-      }
       },
   error=function(cond){
     message("No keys are found!")
