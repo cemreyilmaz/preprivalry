15b47adb7a36e4c6328ede1f9169b1621df12898
cemreyilmaz <cemreyilmaz@yahoo.com>
2023-01-26 14:49:05 +0100

    sync

--- tests/testthat/test-output_classes.R
+++ tests/testthat/test-output_classes.R
@@ -32,10 +32,9 @@ b[[3]] <- data.frame(key = c(0, 0, 0, 0, 0, 0, 0),
                      eye_info = c("Transition","Transition","Transition","Transition","Transition","Transition","Transition"))
 run_data <- list(a,b)
 
+setwd("..")
+setwd("..")
 directory <- paste0(getwd(),'/data')
-experiments <- c("Gratings", "Images","Dots")
-subjects <- c("s001","s002","s003")
-sessions <- 1:3
 
 test_that("clean_keyevents returns a data.frame", {expect_true(is.data.frame(
   clean_keyevents(data.frame(idUp = c(114,114,114,115,115),
@@ -71,15 +70,15 @@ test_that("descriptive_trial returns matrix", {expect_true(is.matrix(
 ))})
 
 test_that("extract_exp returns data.frame", {expect_true(is.data.frame(
-  extract_exp(read_rivdata(directory,experiments[1],subjects[1],sessions[1]))
+  extract_exp(read_rivdata(directory,"Gratings","s001",1))
 ))})
 
 test_that("extract_key returns data.frame", {expect_true(is.data.frame(
-  extract_key(read_rivdata(directory,experiments[1],subjects[1],sessions[1]))
+  extract_key(read_rivdata(directory,"Gratings","s001",1))
 ))})
 
-exp<-extract_exp(read_rivdata(directory,experiments[1],subjects[1],sessions[1]))
-key<-extract_key(read_rivdata(directory,experiments[1],subjects[1],sessions[1]))
+exp<-extract_exp(read_rivdata(directory,"Gratings","s001",1))
+key<-extract_key(read_rivdata(directory,"Gratings","s001",1))
 test_that("extract_trialkey returns a list", {
   expect_true(is.data.frame(extract_trialkey(key,exp[1,])))
 })
@@ -93,15 +92,15 @@ test_that("preprocessing_run returns a list", {
 })
 
 test_that("preprocessing_session returns a list", {
-  expect_true(is.list(preprocessing_session(directory,experiments[1],subjects[1],sessions[1])))
+  expect_true(is.list(preprocessing_session(directory,c("Gratings","Images","GratingImage"),"s001",1)))
 })
 
 test_that("preprocessing_subject returns a list", {
-  expect_true(is.list(preprocessing_subject(directory,experiments[1],subjects[1],sessions[3])))
+  expect_true(is.list(preprocessing_subject(directory,"Gratings","s001",3)))
 })
 
 test_that("read_riv returns a list", {
-  expect_true(is.list(read_rivdata(directory,experiments[1],subjects[1],sessions[1])))
+  expect_true(is.list(read_rivdata(directory,"Gratings","s001",1)))
 })
 
 test_that("remove_irrelevant_keyevents returns data.frame", {expect_true(is.data.frame(
@@ -119,11 +118,11 @@ test_that("reorganize_as_table returns a data.frame", {
 })
 
 test_that("replay_score returns numeric", {expect_true(is.numeric(
-  replay_score(rescale_replay_data(directory, experiments[1], subjects[1], sessions[3]))
+  replay_score(rescale_replay_data(directory,"Gratings","s001",3))
 ))})
 
 test_that("rescale_replay_data returns data.frame", {expect_true(is.data.frame(
-  rescale_replay_data(directory, experiments[1], subjects[1], sessions[3])
+  rescale_replay_data(directory,"Gratings","s001",3)
 ))})
 
 test_that("rivalry.eye_dominance returns numeric", {expect_true(is.numeric(
